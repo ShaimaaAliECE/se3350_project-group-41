37f530cb6e4c7c35539314b404e5a854d6c7a843
csam11 <71097011+csam11@users.noreply.github.com>
2022-02-01 19:46:23 -0500

    creating post app in express

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.urlencoded({extended:true}))
+
 // serve static contents
 app.use(express.static('static'));
+
+app.post('/', (req, res) => {
+    var arr = [];
+    while(arr.length < 10){
+        var r = Math.floor(Math.random() * 20) + 1;
+        if(arr.indexOf(r) === -1) arr.push(r);
+    }
+});
+
+
 
 
 
