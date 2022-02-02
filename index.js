6c5b699db4f837185dc27396d59bff006661a065
csam11 <71097011+csam11@users.noreply.github.com>
2022-02-01 20:30:50 -0500

    rng

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const app = express();
 // serve static contents
 app.use(express.static('static'));
 
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
     var arr = [];
+    arr = req.body.random;
     while(arr.length < 10){
         var r = Math.floor(Math.random() * 20) + 1;
         if(arr.indexOf(r) === -1) arr.push(r);
