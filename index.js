2aec3593c38e9eb604b3a3a5bfe471baa642fa7b
suddin25 <suddin25@uwo.ca>
2022-02-17 17:24:23 -0500

    commit 2

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const {request} = require('express');
-const express = require('express');
+const { request } = require("express");
+const express = require("express");
 const app = express();
 const port = 1000;
-app.use(express.urlencoded({ extended: true}))
-app.use(express.static('static'));
-app.listen(port, function() {
-  console.log(`Example app listening on port ${port}!`)
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("static"));
+
+app.listen(port, function () {
+	console.log(`Example app listening on port ${port}!`);
 });
-var questions =  [
-  { prompt : "What is the next step",
-   answer : "idk", //use a documeent selector to get the answer from html
- }
- ];
-  
- for( var i =0; i < questions.length; i++)
- {
-   var respone = window.prompt(questions[i].prompt)
-   if (response == questions[i].answer)
-   {
-     alert("Correct"); //add sound effects here
-   }else
-   {
-     alert("Wrong");
-   }
- }
+
+/* var questions = [
+	{
+		prompt: "What is the next step",
+		answer: "idk", //use a documeent selector to get the answer from html
+	},
+];
+
+for (var i = 0; i < questions.length; i++) {
+	var response = window.prompt(questions[i].prompt);
+	if (response == questions[i].answer) {
+		alert("Correct"); //add sound effects here
+	} else {
+		alert("Wrong");
+	}
+} */
