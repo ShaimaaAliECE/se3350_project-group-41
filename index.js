e87126bf2e8b79b56328f10b14e2ad2dc18ec80e
DanielMaximos7 <dmaximos@uwo.ca>
2022-03-24 17:49:03 -0400

    nedb created
    was able to store streak and score

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const { request } = require("express");
 const express = require("express");
 const Datastore = require("nedb");
 
+
 const database = new Datastore('scoreboard.db');
 database.loadDatabase();
-
 
 
 
@@ -12,11 +12,24 @@ const app = express();
 
 app.use(express.static("./static"));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const port = process.env.PORT || 8080;
 
 app.listen(port, function () {
 	console.log(`Example app listening on port ${port}!`);
+});
+
+app.post('/db', (req,res)=>{
+	const { score, streak } = req.body;
+
+	console.log("cLL");
+	console.log(score);
+	database.insert({score: score, streak: streak}, function (err, doc) {
+		if (err) {
+		  console.log(err);
+		}
+	  });
 });
 
 
