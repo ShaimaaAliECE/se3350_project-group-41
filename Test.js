1b5f7d0abd2f59476799d5fb215d884c7b0483d0
suddin25 <suddin25@uwo.ca>
2022-03-28 17:29:48 -0400

    Commit 2

--- Test.js
+++ Test.js
@@ -1,7 +1,80 @@
 // @ts-ignore
+class BinaryTreeNode {
+	constructor(value, parent = null) {
+		this.value = value;
+		this.parent = parent;
+		// Descendents are nodes
+		this.left = null;
+		this.right = null;
+		this.step = '';
+	}
 
-let structure = require('./BinaryTree');
-let BinaryTreeNode = require('./BinaryTree');
+	getLeft() {
+		return this.left;
+	}
+
+	setLeft(node) {
+		this.left = node;
+		if (node) {
+			node.parent = this;
+		}
+		node.step = this.step + 'L';
+		// console.log(this.step);
+		// console.log(this.getLeft().value);
+	}
+
+	setRight(node) {
+		this.right = node;
+		if (node) {
+			node.parent = this;
+		}
+		node.step = this.step + 'R';
+		// console.log(this.step);
+		// console.log(this.getRight().value);
+	}
+
+	getRight() {
+		return this.right;
+	}
+
+	getStep() {
+		return this.step;
+	}
+}
+
+class BinaryTree {
+	constructor(value) {
+		this.root = new BinaryTreeNode(value);
+	}
+
+	*inOrderTraversal(node = this.root) {
+		if (node.left) yield* this.inOrderTraversal(node.left);
+		yield node;
+		if (node.right) yield* this.inOrderTraversal(node.right);
+	}
+
+	*postOrderTraversal(node = this.root) {
+		if (node.left) yield* this.postOrderTraversal(node.left);
+		if (node.right) yield* this.postOrderTraversal(node.right);
+		yield node;
+	}
+
+	*preOrderTraversal(node = this.root) {
+		yield node;
+		if (node.left) yield* this.preOrderTraversal(node.left);
+		if (node.right) yield* this.preOrderTraversal(node.right);
+	}
+}
+
+/* 
+
+		DIFFERENT FILE BELOW
+
+
+*/
+
+/* Import BinaryTree and BinaryTreeNode classes
+let BinaryTree = require('./BinaryTree'); */
 
 // Array of random numbers
 let nums = [];
@@ -11,9 +84,15 @@ for (let i = 0; i < 10; i++) {
 	nums.push(a);
 }
 
-// console.log(nums);
+// Return the generate array of random numbers
+export function getNumArray() {
+	return nums;
+}
 
-let tree = new structure.BinaryTree(nums);
+// Initilize parent node
+let tree = new BinaryTree(nums);
+
+//  List for entires of nodes, their values and step
 let entries = [];
 
 // Merge sort
@@ -46,21 +125,35 @@ function mergeSort(unsortedArray, index, flag, node) {
 		// console.log(entry);
 	}
 
-	// console.log('Divide the array in half');
-	// console.log('\nSplit #' + count + flag.substring(count - 1) + '    Left: ' + left + '    Right: ' + right + '    Flag: ' + flag);
-	// enterSplit(left, count, flag + '0');
-	// enterSplit(right, count, flag + '1');
+	/* console.log('Divide the array in half');
+	console.log('\nSplit #' + count + flag.substring(count - 1) + '    Left: ' + left + '    Right: ' + right + '    Flag: ' + flag);
+	enterSplit(left, count, flag + '0');
+	enterSplit(right, count, flag + '1');
 
-	// let L = new BinaryTreeNode(mergeSort(left, count, flag + '0'));
-	// let R = new BinaryTreeNode(mergeSort(right, count, flag + '1'));
+	let L = new BinaryTreeNode(mergeSort(left, count, flag + '0'));
+	let R = new BinaryTreeNode(mergeSort(right, count, flag + '1')); */
 
-	let L = new structure.BinaryTreeNode(left);
-	let R = new structure.BinaryTreeNode(right);
+	let L = new BinaryTreeNode(left);
+	let R = new BinaryTreeNode(right);
 
 	node.setLeft(L);
 	node.setRight(R);
 
-	console.log(node.getLeft().value, node.getLeft().step, node.getRight().value, node.getRight().step);
+	let leftEntry = {
+		value: node.getLeft().value,
+		step: node.getLeft().step,
+	};
+
+	entries.push(leftEntry);
+
+	let rightEntry = {
+		value: node.getRight().value,
+		step: node.getRight().step,
+	};
+
+	entries.push(rightEntry);
+
+	// console.log(node.getLeft().value, node.getLeft().step, node.getRight().value, node.getRight().step);
 
 	// Using recursion to combine the (left = 0) and (right = 1)
 	return merge(mergeSort(left, count, flag + '0', node.getLeft()), mergeSort(right, count, flag + '1', node.getRight()));
@@ -90,15 +183,15 @@ function merge(left, right) {
 // console.log(mergeSort(nums, 0, 'S'));
 
 mergeSort(nums, 0, '', tree.root);
+
 // console.log(entries);
 
-// console.log(Math.round(Math.log(20) / Math.log(2) + 1));
 let depth = Math.round(Math.log(20) / Math.log(2) + 1);
 
 // console.log(entries.sort((a, b) => (a.count > b.count ? 1 : -1)));
 // console.log(entries.sort((a, b) => (a.flag.length > b.flag.length ? 1 : -1)));
 
-let sortedList = [];
+// let sortedList = [];
 
 /* for (let i = 1; i < depth; i++) {
 	// Used to save the sub array
@@ -125,4 +218,8 @@ let sortedList = [];
 
 // console.log(sortedList);
 
-// [...tree.preOrderTraversal()].map((x) => x.value);
+export function getEntries() {
+	return entries;
+}
+
+console.log(entries);
