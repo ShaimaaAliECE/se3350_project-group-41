afbc389a5ed8b14b11d56811fd1e4d66edb61545
DanielMaximos7 <dmaximos@uwo.ca>
2022-03-28 22:17:10 -0400

    tweaks

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ app.listen(port, function () {
 let playerName  = "";
 let lvl2sStreak = 0;
 let lvl2sScore = 0;
-let lvl2Atempts = 0;
+let lvl2Attempts = 0;
 let lvl3sScore = 0;
 let lvl3sStreak = 0;
-let lvl3Atempts = 0;
+let lvl3Attempts = 0;
 
 let final = 0;
 
@@ -35,35 +35,47 @@ app.post('/finalcall', (req,res)=>{
 
 	final = finalbool;
 
+
+});
+
+
+if(final = 1){
+	app.get('/write',(req,res) =>{
+
+		console.log("write");
+		res.json({
+			playerName: playerName,
+			lvl2sScore: lvl2sScore,
+			lvl2Attempts: lvl2Attempts,
+			lvl3sScore: lvl3sScore,
+			lvl3Attempts: lvl3Attempts,
 	
-
-});
-
-
-
-app.get('/write',(req,res) =>{
-
-	console.log("write");
-	res.json({
-		playerName: playerName,
-		lvl2sScore: lvl2sScore,
-		lvl2sStreak: lvl2sStreak,
-		lvl3sScore: lvl3sScore,
-		lvl3sStreak: lvl3sStreak,
-
+			
+		});
+		//inserts all data into the database
 		
+			database.insert({name: playerName, lvl2: lvl2sScore, lvl2A: lvl2Attempts, 
+				lvl3: lvl3sScore, lvl3A: lvl3Attempts});
+		
+	
+	
 	});
-	//inserts all data into the database
-	database.insert({name: playerName, lvl2: lvl2sScore, lvl2A: lvl2Atempts, 
-		lvl3: lvl3sScore, lvl3A: lvl3Atempts});
+}
 
-});
 
 app.get('/finallvl', (req,res)=>{
 
 	res.json({
 
-		final: finalbool,
+		
+		if(err){
+
+			res.end();
+			return;
+
+		},
+
+		// final: finalbool
 
 	})
 	//database.insert({playername: name});
@@ -84,7 +96,7 @@ app.post('/lvlthree', (req,res)=>{
 	const { score, mistakes  } = req.body;
 
 	lvl3sScore = score;
-	lvl3sStreak = streak;
+	lvl3Attempts = mistakes;
 
 });
 
@@ -93,7 +105,9 @@ app.post('/lvltwo', (req,res)=>{
 	const { score, mistakes } = req.body;
 
 	lvl2sScore = score;
-	lvl2Atempts = mistakes;
+	lvl2Attempts = mistakes;
+
+	console.log(mistakes);
 	
 	//database.insert({score: score, streak: streak}, function (err, doc) {
 	//	if (err) {
@@ -104,7 +118,7 @@ app.post('/lvltwo', (req,res)=>{
 
 
 app.get('/scores', (req, res) => {
-
+	console.log("call");
 	database.find({}, (err, data) => {
 
 		if(err){
